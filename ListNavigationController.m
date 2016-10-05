65a9ab60e3a611d389488342f4e98a365165ee35
mymatrixisbroken <joaquinlfeliciano@gmail.com>
2016-10-05 15:44:48 -0700

    Add files via upload

--- ListNavigationController.m
+++ ListNavigationController.m
@@ -18,11 +18,12 @@
     [UINavigationBar appearance].tintColor = [UIColor whiteColor];
     UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menu"] style:UIBarButtonItemStylePlain target:self   action:@selector(barButtonCustomPressed:)];
     UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"arrow_left"] style:UIBarButtonItemStylePlain target:self   action:@selector(leftButtonPressed:)];
-    self.navigationBar.topItem.rightBarButtonItem = rightButton;
+//    self.navigationBar.topItem.rightBarButtonItem = rightButton;
     self.navigationBar.topItem.leftBarButtonItem = leftButton;
 }
 
 -(IBAction) leftButtonPressed:(UIBarButtonItem *)btn{
+    objectsArray.searchType = 0;
     [user goToNewsFeedViewController:self];
 }
 
